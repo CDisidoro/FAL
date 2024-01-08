bf6f2cbdaf5d5bcff64f8b620637ac095ef07934
CDisidoro <cdisidoro99@gmail.com>
2024-01-08 16:24:49 +0100

    Ejercicio Soprino resuelto (Correct en juez)

--- Ejercicios/22 - Los soprino/Codigo.cpp
+++ Ejercicios/22 - Los soprino/Codigo.cpp
@@ -20,7 +20,6 @@ sirven cada uno de ellos
 	posPersona (Entrada): Posicion de la siguiente persona a asignar y controlar que llegamos al final de la lista de personas
 	sobrios[] (Entrada): Cantidad de sobrios en el vehiculo X
 	ebrios[] (Entrada): Cantidad de ebrios en el vehiculo X
-	asignaciones[] (Entrada): Lista de asignaciones (asignaciones[X] = Y => La persona X esta en el vehiculo Y)
 	soluciones (Salida): Cantidad de soluciones posibles
 (2) ¿Cómo son las soluciones parciales de este problema?
 	Una lista de asignaciones de personas a vehiculos donde no hemos terminado de asignar a todas las personas y vehiculos
@@ -36,35 +35,32 @@ parciales viables?
 	Y también se plantea no alojar el pasajero en el vehiculo X sino que lo movemos al X+1, haya espacio o no en el vehiculo X
 (6) Análisis de casos
 	(6.1) Caso(s) base
-		if (posVehiculo == datos.n_vehiculos || posPersona == datos.n_personas) {
+		if (posPersona == datos.n_personas) {
 			if (esViable(datos, solucion)) {
 				soluciones++;
 			}
 		}
 	(6.2) Caso(s) recursivos
-		else {
-			int puestosOcupados = solucion.sobrios[posVehiculo] + solucion.ebrios[posVehiculo];
-			if (puestosOcupados + 1 <= datos.capacidad[posVehiculo]) {
-				for (int persona = 0; persona < datos.n_personas; persona++) {
-					if (solucion.asignaciones[persona] == -1) {
-						if (datos.ha_bebido[persona] && solucion.ebrios[posVehiculo] + 1 <= (datos.capacidad[posVehiculo] / 2)) {
-							solucion.ebrios[posVehiculo]++;
-							solucion.asignaciones[persona] = posVehiculo;
-							solve(datos, solucion, posVehiculo, posPersona + 1, soluciones);
-							solucion.asignaciones[persona] = -1;
-							solucion.ebrios[posVehiculo]--;
-						}
-						else if (!datos.ha_bebido[persona]) {
-							solucion.sobrios[posVehiculo]++;
-							solucion.asignaciones[persona] = posVehiculo;
-							solve(datos, solucion, posVehiculo, posPersona + 1, soluciones);
-							solucion.asignaciones[persona] = -1;
-							solucion.sobrios[posVehiculo]--;
-						}
+		} else {
+			for (int posVehiculo = 0; posVehiculo < datos.n_vehiculos; posVehiculo++) { //Recorremos entera la lista de vehiculos
+				int puestosOcupados = solucion.sobrios[posVehiculo] + solucion.ebrios[posVehiculo];
+				if (puestosOcupados < datos.capacidad[posVehiculo]) { //Si hay plazas suficientes en el vehiculo se prueba alojar gente en este vehiculo
+					if (datos.ha_bebido[posPersona] && solucion.ebrios[posVehiculo] + 1 <= (datos.capacidad[posVehiculo] / 2)) { //Si ha bebido y al asignarlo no se supera el limite de ebrios
+						solucion.ebrios[posVehiculo]++;
+						posPersona++;
+						solve(datos, solucion, posPersona, soluciones); //Recursion con la persona X+1
+						posPersona--;
+						solucion.ebrios[posVehiculo]--;
+					}
+					else if (!datos.ha_bebido[posPersona]) { //Si la persona no ha bebido
+						solucion.sobrios[posVehiculo]++;
+						posPersona++;
+						solve(datos, solucion, posPersona, soluciones); //Recursion con la persona X+1
+						posPersona--;
+						solucion.sobrios[posVehiculo]--;
 					}
 				}
 			}
-			solve(datos, solucion, posVehiculo + 1, posPersona, soluciones);
 		}
 (7) En caso de utilizar una generalización, explicar cómo se define el algoritmo final
 a partir de la misma, por inmersión.
@@ -73,9 +69,6 @@ a partir de la misma, por inmersión.
 	for (int i = 0; i < datos.n_vehiculos; i++) {
 		solucion.ebrios[i] = 0;
 		solucion.sobrios[i] = 0;
-	}
-	for (int i = 0; i < datos.n_personas; i++) {
-		solucion.asignaciones[i] = -1;
 	}
 	solve(datos, solucion, 0, 0, soluciones);
 */
@@ -96,30 +89,27 @@ bool esViable(const tDatos& datos, const tSol& solucion) {
 }
 
 void solve(const tDatos& datos, tSol solucion, int& posPersona, int& soluciones) {
-	if (posPersona == datos.n_personas) { //Si ha pasado por todas las personas o vehiculos
+	if (posPersona == datos.n_personas) { //Si llegamos al final de la lista de personas
 		if (esViable(datos, solucion)) {
 			soluciones++;
 		}
-	}else {
-		for (int persona = posPersona; persona < datos.n_personas; persona++) {
-			for (int posVehiculo = 0; posVehiculo < datos.n_vehiculos; posVehiculo++) {
-				int puestosOcupados = solucion.sobrios[posVehiculo] + solucion.ebrios[posVehiculo];
-				//Si hay plazas suficientes en el vehiculo se prueba alojar gente en este vehiculo
-				if (puestosOcupados < datos.capacidad[posVehiculo]) {
-					if (datos.ha_bebido[persona] && solucion.ebrios[posVehiculo] + 1 <= (datos.capacidad[posVehiculo] / 2)) { //Si ha bebido y al asignarlo no se supera el limite de ebrios
-						solucion.ebrios[posVehiculo]++;
-						posPersona++;
-						solve(datos, solucion, posPersona, soluciones);
-						posPersona--;
-						solucion.ebrios[posVehiculo]--;
-					}
-					else if (!datos.ha_bebido[persona]) { //Si la persona no ha bebido
-						solucion.sobrios[posVehiculo]++;
-						posPersona++;
-						solve(datos, solucion, posPersona, soluciones);
-						posPersona--;
-						solucion.sobrios[posVehiculo]--;
-					}
+	} else {
+		for (int posVehiculo = 0; posVehiculo < datos.n_vehiculos; posVehiculo++) { //Recorremos entera la lista de vehiculos
+			int puestosOcupados = solucion.sobrios[posVehiculo] + solucion.ebrios[posVehiculo];
+			if (puestosOcupados < datos.capacidad[posVehiculo]) { //Si hay plazas suficientes en el vehiculo se prueba alojar gente en este vehiculo
+				if (datos.ha_bebido[posPersona] && solucion.ebrios[posVehiculo] + 1 <= (datos.capacidad[posVehiculo] / 2)) { //Si ha bebido y al asignarlo no se supera el limite de ebrios
+					solucion.ebrios[posVehiculo]++;
+					posPersona++;
+					solve(datos, solucion, posPersona, soluciones); //Recursion con la persona X+1
+					posPersona--;
+					solucion.ebrios[posVehiculo]--;
+				}
+				else if (!datos.ha_bebido[posPersona]) { //Si la persona no ha bebido
+					solucion.sobrios[posVehiculo]++;
+					posPersona++;
+					solve(datos, solucion, posPersona, soluciones); //Recursion con la persona X+1
+					posPersona--;
+					solucion.sobrios[posVehiculo]--;
 				}
 			}
 		}
