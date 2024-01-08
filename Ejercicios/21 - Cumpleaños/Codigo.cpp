01e39519d1f8c610244332fa3ff6502b3575bc95
CDisidoro <cdisidoro99@gmail.com>
2024-01-08 21:08:50 +0100

    Actualizacion solucion Cumpleaños (Timelimit en Juez)

--- Ejercicios/21 - Cumpleaños/Codigo.cpp
+++ Ejercicios/21 - Cumpleaños/Codigo.cpp
@@ -1,3 +1,4 @@
+//Iteracion 08/01/24
 #include <iostream>
 using namespace std;
 
@@ -10,15 +11,13 @@ const unsigned int MAX_NINIOS = 20; // maximo numero de ninos
 typedef struct {
 	int n_ninios;	// numero de ninos.
 	int n_sitios;	// numero de sitios
-	int sat[MAX_NINIOS][MAX_NINIOS];  // la posicion i,j representa la satisfaccion
-	// que siente el ninio i si se le asigna el sitio j
+	int sat[MAX_NINIOS][MAX_NINIOS];  // la posicion sitio,j representa la satisfaccion
+	// que siente el ninio sitio si se le asigna el sitio j
 } tMatrizSatisfacciones;
 
 typedef struct {
-	int puestosOcupados;
 	int asignaciones[MAX_NINIOS];
 	int sumaSatisfaccion;
-	int numNinio;
 } tDatos;
 
 /*
@@ -39,76 +38,68 @@ parciales viables?
 
 	(6.2) Caso(s) recursivos
 
-(7) En caso de utilizar una generalizacion, explicar c�mo se define el algoritmo final
+(7) En caso de utilizar una generalizacion, explicar como se define el algoritmo final
 a partir de la misma, por inmersion.
 
 */
-void solve(const tMatrizSatisfacciones& sats, int posHermano, const int limiteMalAsignados, tDatos datos, int& maxSatisfaccion, int malAsignados, bool estaHermano) {
-	//Si hemos llegado al final de la lista de ninios o ya hemos ocupado todos los asientos llegamos al caso base
-	if (datos.numNinio == sats.n_ninios || datos.puestosOcupados == sats.n_sitios) {
+void solve(const tMatrizSatisfacciones& sats, int posHermano, const int limiteMalAsignados, tDatos datos, int& maxSatisfaccion, int& malAsignados, bool& estaHermano, int numNinio) {
+	if(numNinio == sats.n_ninios){
 		//Para validar la solucion final comprobamos que este presente el hermano, y que la satisfaccion total de esta combinacion supere la maxima hallada hasta ahora
 		if (estaHermano && datos.sumaSatisfaccion > maxSatisfaccion) {
 			maxSatisfaccion = datos.sumaSatisfaccion;
 		}
 	} else {
-		//Para el caso recursivo ejecutamos el bucle for mientras no lleguemos al final de la lista de ninios y que hayan puestos libres
-		//No se pone que parta desde datos.numNinio porque limitaria demasiado la busqueda (Sentar al ninio X en el asiento Y haria que ignorasemos la afinidad de los ninios [0...X-1] para el asiento Y+1)
-		for (int i = 0; i < sats.n_ninios; i++) {
-			//Si la afinidad del ninio por ese puesto es positiva lo agrega a la solucion parcial
-			if (datos.asignaciones[datos.puestosOcupados] < 0 && sats.sat[i][datos.puestosOcupados] > -1) {
+		//Para el caso recursivo ejecutamos el bucle for para recorrer la lista de sitios
+		for (int sitio = 0; sitio < sats.n_sitios; sitio++) {
+			//        Puesto libre                    Afinidad positiva
+			if (datos.asignaciones[sitio] < 0 && sats.sat[numNinio][sitio] >= 0) {
 				//Aplicamos marcaje:
 				/*
-					Si el ninio que estamos agregando es el hermano activamos el booleano de estaHermano
 					Si la asignacion del ninio es mala incrementamos el contador de malAsignados
-					Al array de asignaciones de puestos le asignaremos el ninio i
+					Si el ninio que estamos agregando es el hermano activamos el booleano de estaHermano
+					Al array de asignaciones de puestos le asignaremos al puesto sitio el ninio numNinio
 					A la suma de satisfaccion le agregaremos la satisfaccion del ninio por ese puesto
 					Aumentamos el numero de ninio
 					Aumentamos el numero de puestos ocupados
 				*/
-				if (i % sats.n_sitios == datos.puestosOcupados) {
+				if (numNinio % sats.n_sitios == sitio) {
 					malAsignados++;
 				}
 				//Si no superamos el limite de mal asignados seguimos con el marcaje, en caso contrario ya no tendria sentido probar esta solucion
 				if (malAsignados <= limiteMalAsignados) {
-					if (i == posHermano) {
+					if (numNinio == posHermano) {
 						estaHermano = true;
 					}
-					datos.asignaciones[datos.puestosOcupados] = i;
-					datos.sumaSatisfaccion += sats.sat[i][datos.puestosOcupados];
-					datos.numNinio++;
-					datos.puestosOcupados++;
-					solve(sats, posHermano, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano); //Se llama a la recursion
-					//Deshacemos el marcaje (Probar a no invitar al ninio X sino al X+1)
-					datos.puestosOcupados--;
-					datos.numNinio--;
-					datos.sumaSatisfaccion -= sats.sat[i][datos.puestosOcupados];
-					datos.asignaciones[datos.puestosOcupados] = -1;
-					if (i == posHermano) {
+					datos.asignaciones[sitio] = numNinio;
+					datos.sumaSatisfaccion += sats.sat[numNinio][sitio];
+					solve(sats, posHermano, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano, numNinio + 1); //Se llama a la recursion
+					//Deshacemos el marcaje
+					datos.asignaciones[sitio] = -1;
+					datos.sumaSatisfaccion -= sats.sat[numNinio][sitio];
+					if (numNinio == posHermano) {
 						estaHermano = false;
 					}
 				}
-				if (i % sats.n_sitios == datos.puestosOcupados) {
+
+				if (numNinio % sats.n_sitios == sitio) {
 					malAsignados--;
 				}
 			}
 		}
-		if (datos.puestosOcupados < sats.n_sitios) {
-			datos.numNinio = sats.n_ninios;
-			solve(sats, posHermano, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano);
-		}
+		//Probamos a no invitar al ninio y pasamos al siguiente
+		solve(sats, posHermano, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano, numNinio + 1);
 	}
 }
 
 int satisfaccion_maxima(const tMatrizSatisfacciones& sats, int h) {
-	int maxSatisfaccion = 0;
-	int malAsignados = 0;
-	int limiteMalAsignados = sats.n_sitios / 3;
-	bool estaHermano = false;
 	tDatos datos;
-	datos.puestosOcupados = 0;
+	int maxSatisfaccion = 0, malAsignados = 0, numNinio = 0, limiteMalAsignados = sats.n_sitios / 3;
+	bool estaHermano = false;
 	datos.sumaSatisfaccion = 0;
-	datos.numNinio = 0;
-	solve(sats, h, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano);
+	for (int i = 0; i < sats.n_sitios; i++) {
+		datos.asignaciones[i] = -1;
+	}
+	solve(sats, h, limiteMalAsignados, datos, maxSatisfaccion, malAsignados, estaHermano, numNinio);
 	return maxSatisfaccion;
 }
 
